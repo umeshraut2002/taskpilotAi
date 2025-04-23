d485602fbd6b74eb2b9b8ae2bcdcc34381a720a2
umesh raut <uraut6044@gmail.com>
2025-04-23 22:37:22 +0530

    controllers created

--- src/models/projectMember.models.js
+++ src/models/projectMember.models.js
@@ -1,5 +1,23 @@
 import mongoose, {Schema} from "mongoose";
+import { type } from "os";
 
-const projectMemberSchema = new Schema({})
+const projectMemberSchema = new Schema({
+
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        require: true
+    },
+
+    project: {
+        type: String,
+        require: true
+    },
+
+    role: {
+        
+    }
+
+}, {timestamps: true})
 
 export const ProjectMember = mongoose.model("ProjectMember", projectMemberSchema);
