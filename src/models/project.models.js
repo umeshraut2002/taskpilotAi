90a6c1c545be1ab1461aeb768a209e7d196d92b8
umesh raut <uraut6044@gmail.com>
2025-04-23 21:42:29 +0530

    project schema created

--- src/models/project.models.js
+++ src/models/project.models.js
@@ -1,5 +1,26 @@
 import mongoose, {Schema} from "mongoose";
+import { type } from "os";
 
-const projectSchema = new Schema({})
+const projectSchema = new Schema({
+
+    name: {
+        type: String,
+        require: true,
+        uinique: true,
+        trim: true
+    },
+
+    description: {
+        type: String,
+        require: true
+    },
+
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        require: true
+    }
+
+}, {timestamps: true})
 
 export const Project = mongoose.model("Project", projectSchema);
