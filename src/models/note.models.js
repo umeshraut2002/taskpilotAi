18a6a2d9ec371bfa4f7d1e6934b673d5f3ec9974
umesh raut <uraut6044@gmail.com>
2025-04-23 21:19:38 +0530

    note schema created

--- src/models/note.models.js
+++ src/models/note.models.js
@@ -1,5 +1,26 @@
+import { timeStamp } from "console";
 import mongoose, {mongo, Schema} from "mongoose";
+import { type } from "os";
 
-const noteSchema = new Schema({})
+const noteSchema = new Schema({
+
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        require: true
+    },
+
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        require: true
+    },
+
+    content: {
+        type: String,
+        require: true
+    }
+
+}, {timestamps: true})
 
 export const Note = mongoose.model("Note", noteSchema);
